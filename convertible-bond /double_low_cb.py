e03b423a6560835865a8225a720eccd7c0490b74
黑胡子(梁立灿) <teach@qcwlink.com>
2021-04-12 17:50:23 +0800

    添加高年化收益率筛选规则

--- convertible-bond /double_low_cb.py
+++ convertible-bond /double_low_cb.py
@@ -1,12 +1,23 @@
- #i['cell']['btype'] = C为可转债，E为可交换债
+### coding:UTF-8
+# import sys
+# reload(sys)
+# sys.setdefaultencoding('utf-8')
+
 import requests
 import json
+# from requests_html import HTMLSession
+
 url = 'https://www.jisilu.cn/data/cbnew/cb_list/?___jsl=LST___'
 response = requests.get(url)
 js=response.json()
-dl_cblist = []   # 双低
-lpp_cblist = []  # 低溢价
-lp_cblist = []  # 低价
+
+# session = HTMLSession()
+# r = session.get(url)
+# print(r.html.text)
+
+dl_cblist  = [] # 双低
+lpp_cblist = [] # 低溢价
+lp_cblist  = [] # 低价
 
 for i in js['rows']:
 	#攻守兼备:双低筛选规则
@@ -42,34 +53,37 @@ for i in js['rows']:
 		lpp_cblist.append(tmp)
 
 	#防守型：低价筛选规则
-	# 1.未强赎
-	# 2.有回售
-	# 3.100 <= 价格 < 105 且溢价率小于15%
-	# 4. 90 <= 价格 < 100 且溢价率小于20%
+	# 1. 未强赎
+	# 2. 有回售
+	# 3. PB > 1, 100 <= 价格 < 105 且溢价率小于15%
+	# 4. PB > 0.8, 90 <= 价格 < 100 且溢价率小于20%
 	# 5. 85 <= 价格 < 90  且溢价率小于25%
 	# 6. 80 <= 价格 < 85  且溢价率小于30%
 	# 7. 价格 < 80        且溢价率小于50%
+	# 8. 评级AA以上，税后年化收益7%以上
 		if i['cell']['put_price'] != None\
-			and ( (100 <= float(i['cell']['price']) < 105 and float(i['cell']['premium_rt'].strip('%')) < 15) \
+			and ( (100 <= float(i['cell']['price']) < 105 and float(i['cell']['premium_rt'].strip('%')) < 15 ) \
 				or (90 <= float(i['cell']['price']) < 100 and float(i['cell']['premium_rt'].strip('%')) < 20) \
 				or (85 <= float(i['cell']['price']) < 90 and float(i['cell']['premium_rt'].strip('%')) < 25) \
 				or (80 <= float(i['cell']['price']) < 85 and float(i['cell']['premium_rt'].strip('%')) < 30) \
-				or (float(i['cell']['price']) < 80 and float(i['cell']['premium_rt'].strip('%')) < 50)):
-			tmp=['现价:',round(float(i['cell']['price']),1), i['cell']['bond_nm'], '债券代码:'+i['id'], '溢价率:'+i['cell']['premium_rt']]
+				or (float(i['cell']['price']) < 80 and float(i['cell']['premium_rt'].strip('%')) < 50) \
+				or i['cell']['rating_cd'].find("AA") != -1 and i['cell']['ytm_rt'].find("-") == -1 and float(i['cell']['ytm_rt'].strip('%')) > 7):
+			tmp=['现价:',round(float(i['cell']['price']),1), i['cell']['bond_nm'], '债券代码:'+i['id'], '溢价率:'+i['cell']['premium_rt'], '税后收益率:'+i['cell']['ytm_rt']]
 			lp_cblist.append(tmp)
 
 print('攻守兼备:双低')
 Num = 0
 dl_cblist.sort()
-for i in dl_cblist:
+for i in dl_cblist[0:10]:
 	Num += 1
 	print('No.%02d %s'% (Num, i))
+	# print('No.%02d %s'% (Num, json.dumps(i).decode("unicode-escape")))
 
 print('--------------------------------')
 print('进攻型：低溢价')
 Num = 0
 lpp_cblist.sort()
-for i in lpp_cblist[0:10]:
+for i in lpp_cblist[0:5]:
 	Num += 1
 	print('No.%02d %s'% (Num, i))
 
@@ -77,6 +91,6 @@ print('--------------------------------')
 print('防守型：低价格')
 Num = 0
 lp_cblist.sort()
-for i in lp_cblist[0:15]:
+for i in lp_cblist[0:10]:
 	Num += 1
 	print('No.%02d %s'% (Num, i))
